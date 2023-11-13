7849519aa55a94b2ef43dbbe7dd63479d64ba7b2
Garry Trinder <garry@trinder365.co.uk>
2023-11-13 15:04:11 +0000

    Add base projects

---
+++ base/tab-spfx/src/src/webparts/tab/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface ITabWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'TabWebPartStrings' {
+  const strings: ITabWebPartStrings;
+  export = strings;
+}
